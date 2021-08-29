1ad43ef5254f7873bce5c4bc562a65ff9c175c08
tinabanthia <71347235+tinabanthia@users.noreply.github.com>
2021-08-29 13:42:26 -0500

    Add files via upload

--- apps/cinder_app_main.cc
+++ apps/cinder_app_main.cc
@@ -1,1 +1,10 @@
+#include <visualizer/minesweeper_app.h>
 
+using minesweeper::visualizer::MinesweeperApp;
+
+void prepareSettings(MinesweeperApp::Settings* settings) {
+    settings->setResizable(false);
+}
+
+// This line is a macro that expands into an "int main()" function.
+CINDER_APP(MinesweeperApp, ci::app::RendererGl, prepareSettings);
